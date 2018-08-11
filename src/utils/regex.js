0bc4997fb333f8b18833ba9bc1de3472a85f0769
yobacca <olaf.ennen@gmail.com>
2018-08-12 00:12:32 +0200

    fix: handle numeric chunks with leading zeros as a string

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -4,7 +4,7 @@ const RE_LEADING_OR_TRAILING_WHITESPACES = /^\s+|\s+$/g; // trim pre-post whites
 const RE_WHITESPACES = /\s+/g; // normalize all whitespace to single ' ' character
 const RE_INT_OR_FLOAT = /^[+-]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?$/; // identify integers and floats
 const RE_DATE = /(^([\w ]+,?[\w ]+)?[\w ]+,?[\w ]+\d+:\d+(:\d+)?[\w ]?|^\d{1,4}[/-]\d{1,4}[/-]\d{1,4}|^\w+, \w+ \d+, \d{4})/; // identify date strings
-const RE_LEADING_ZERO = /^0+[1-9]+$/;
+const RE_LEADING_ZERO = /^0+[1-9]{1}[0-9]*$/;
 const RE_UNICODE_CHARACTERS = /[^\x00-\x80]/;
 
 export {
