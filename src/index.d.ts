cd999a62dda3ff088512d42198f23e9ee2baf085
Thomas den Hollander <denhollander.thomas@gmail.com>
2018-11-12 16:04:46 +0100

    feat(typescript): add typescript declarations file
    closes #1.

---
+++ src/index.d.ts
@@ -0,0 +1,10 @@
+export type Identifier<T> = number | string | ((value: T) => unknown);
+export type Order<T> = 'asc' | 'desc' | ((valueA: T, valueB: T) => number);
+export function orderBy<T>(collection: T[], identifiers?: Array<Identifier<T>> | Identifier<T>, orders?: Array<Order<T>> | Order<T>): T[];
+
+export interface CompareOptions {
+  caseSensitive?: boolean,
+  order?: 'asc' | 'desc'
+}
+export type CompareFn = ((valueA: unknown, valueB: unknown) => number);
+export function compare(options?: CompareOptions): CompareFn;
