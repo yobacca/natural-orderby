9cd56cd0dba2f2456df7a8e488fb166a21634e08
Olaf Ennen <yobacca@users.noreply.github.com>
2018-11-18 23:13:07 +0100

    chore(deps): upgraded commitlint (#11)

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      ['feat', 'fix', 'docs', 'refactor', 'test', 'revert'],
+      ['feat', 'fix', 'docs', 'chore', 'refactor', 'test', 'revert'],
     ],
   },
 };
