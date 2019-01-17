e94209484a25fe421dea0e9f85d7cd02707712f8
yobacca <olaf.ennen@gmail.com>
2019-01-17 22:59:55 +0100

    feat(flow): added flow library definition

---
+++ flow-typed/natural-orderby.js
@@ -0,0 +1,36 @@
+type $npm$naturalOrderBy$CompareFn = (valueA: mixed, valueB: mixed) => number;
+
+type $npm$naturalOrderBy$CaseSensitive = boolean;
+
+type $npm$naturalOrderBy$OrderEnum = 'asc' | 'desc';
+
+type $npm$naturalOrderBy$Order =
+  | $npm$naturalOrderBy$OrderEnum
+  | $npm$naturalOrderBy$CompareFn;
+
+type $npm$naturalOrderBy$CompareOptions = {|
+  $npm$naturalOrderBy$CaseSensitive?: $npm$naturalOrderBy$CaseSensitive,
+  order?: $npm$naturalOrderBy$OrderEnum,
+|};
+
+type $npm$naturalOrderBy$IdentifierFn<T> = (value: T) => mixed;
+
+type $npm$naturalOrderBy$Identifier<T> =
+  | $npm$naturalOrderBy$IdentifierFn<T>
+  | string;
+
+declare module 'natural-orderby' {
+  declare export function compare(
+    options?: $npm$naturalOrderBy$CompareOptions
+  ): $npm$naturalOrderBy$CompareFn;
+
+  declare export function orderBy<T>(
+    collection: $ReadOnlyArray<T>,
+    identifiers?:
+      | ?$ReadOnlyArray<$npm$naturalOrderBy$Identifier<T>>
+      | ?$npm$naturalOrderBy$Identifier<T>,
+    orders?:
+      | ?$ReadOnlyArray<$npm$naturalOrderBy$Order>
+      | ?$npm$naturalOrderBy$Order
+  ): Array<T>;
+}
