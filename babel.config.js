0e9d4e57853e9076c4add3e030649fc8faf4f15b
yobacca <olaf.ennen@gmail.com>
2019-01-17 23:13:31 +0100

    chore(build): generate all output formats by rollup

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,5 @@
 /* eslint-disable flowtype/require-valid-file-annotation */
-const { NODE_ENV } = process.env;
-const modules = NODE_ENV === 'cjs' || NODE_ENV === 'test' ? 'commonjs' : false;
-const loose = true;
-
-const presets = [['@babel/preset-env', { loose, modules }]];
+const presets = [['@babel/preset-env', { loose: true }]];
 const plugins = [
   '@babel/plugin-proposal-object-rest-spread',
   '@babel/plugin-transform-flow-strip-types',
